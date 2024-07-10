f4af1fd61686e9a794fe404a678102d985092482
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-07-10 09:12:48 +0545

    ch

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 
 
-export const baseUrl = 'http://192.168.0.105:5000/api';
-export const imageUrl = 'http://192.168.0.105:5000';
+export const baseUrl = 'https://shopback-58dq.onrender.com/api';
+export const imageUrl = 'https://shopback-58dq.onrender.com';
 export const userUrl = `${baseUrl}/users`;
 export const productUrl = `${baseUrl}/products`;
 export const orderUrl = `${baseUrl}/orders`;
